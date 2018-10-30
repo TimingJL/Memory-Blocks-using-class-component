711a2c70967087fefa4da8c627c63a046310e825
Taiming Chen <eefozeo@gmail.com>
2018-10-30 23:38:15 +0800

    Updates

---
+++ precache-manifest.3d8f3fd86c127ac1f1938dc9872739c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "116f8d1be2b00b3e7cc5",
+    "url": "/Memory-Blocks/static/js/runtime~main.116f8d1b.js"
+  },
+  {
+    "revision": "eb41455a8c9f309ea858",
+    "url": "/Memory-Blocks/static/js/main.eb41455a.chunk.js"
+  },
+  {
+    "revision": "bc6a63aa7b9f9cacc8ca",
+    "url": "/Memory-Blocks/static/js/1.bc6a63aa.chunk.js"
+  },
+  {
+    "revision": "eb41455a8c9f309ea858",
+    "url": "/Memory-Blocks/static/css/main.ef5ba0f3.chunk.css"
+  },
+  {
+    "revision": "94b3f91b11790a6f2e16086cace28479",
+    "url": "/Memory-Blocks/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Memory-Blocks/precache-manifest.437e4fdf401c702bdb1cc2bd7b63e2ce.js"
+  "/Memory-Blocks/precache-manifest.3d8f3fd86c127ac1f1938dc9872739c8.js"
 );
 
 workbox.clientsClaim();
